3dbb2c2f11db6f4ddf56a9b55d85756ef5ae0be2
Jeppe Vennekilde <jeppe@jeppes.info>
2014-12-01 01:44:55 +0100

    Added spaces because OCD

--- manager/src/main/java/com/mucommander/job/TransferFileJob.java
+++ manager/src/main/java/com/mucommander/job/TransferFileJob.java
@@ -475,7 +475,7 @@ public abstract class TransferFileJob extends FileJob {
      * @return the size of the file currently being processed, -1 if this information is not available.
      */
     public long getCurrentFileSize() {
-        return getCurrentFile()==null?-1:getCurrentFile().getSize();
+        return getCurrentFile()==null? -1 : getCurrentFile().getSize();
     }
 
 
@@ -627,7 +627,7 @@ public abstract class TransferFileJob extends FileJob {
     public String getStatusString() {
         if(isCheckingIntegrity())
             return Translator.get("progress_dialog.verifying_file", getCurrentFilename());
-
+        
         return super.getStatusString();
     }
     
